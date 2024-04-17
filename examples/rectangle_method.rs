762b2964560e3438cd618910739b5101af5b1c52
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-04-17 20:30:30 +0200

    add trapezoid example & comments to describe steps

--- examples/rectangle_method.rs
+++ examples/rectangle_method.rs
@@ -2,9 +2,13 @@ fn main() {
     let closure = |x: f64| 2.0 * x;
     let step = 0.00001;
     // compute integral over the [0.0; 1.0] range using the right endpoint for rectangles
+    // compute Xs
     let args = (1..100001).map(|step_id| step * step_id as f64);
+    // compute Ys from Xs
     let rets = args.map(closure);
+    // compute rectangle areas
     let rectangles = rets.map(|ret| ret * step);
+    // reduce & print result
     let res: f64 = rectangles.sum();
     println!("integral value of f(x) = 2 * x over [0; 1]: {res}");
 }
