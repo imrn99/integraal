107a7683a002182ca46a9e4ff14d5f0f39826cc6
imrn99 <github@imrn.fr>
2024-04-28 18:12:41 +0200

    update montecarlo to fit the component
    the description for the MC method probably needs to be different from
    rectangles/trapezoids

--- examples/montecarlo_method.rs
+++ examples/montecarlo_method.rs
@@ -1,23 +1,33 @@
 use rand::Rng;
 
 fn main() {
-    let closure = |x: f64| 2.0 * x;
-    const N_STEP: usize = 100_000;
-    const N_SAMPLE: usize = 100;
+    // describe Xs
+    // 100_001 samples
+    const N_SAMPLE: usize = 100_000;
     const STEP: f64 = 0.00001;
+    let args = (0..=N_SAMPLE).map(|step_id| STEP * step_id as f64);
 
-    let args = (0..=N_STEP).map(|step_id| STEP * step_id as f64);
+    // describe Ys
+    let closure = |x: f64| 2.0 * x;
+
+    // describe the Monte Carlo process
+    const N_MONTECARLO_SAMPLE: usize = 100;
+    let mut rng = rand::thread_rng();
+
+    // compute Ys from Xs
     let rets = args.map(closure);
+
+    // compute area from the number of sample under the curve
     let count: usize = rets
         .map(|y| {
-            let mut rng = rand::thread_rng();
             let iter = (&mut rng)
-                .sample_iter::<f64, _>(rand::distributions::Uniform::new(0.0, 2.0))
-                .take(N_SAMPLE);
+                .sample_iter::<f64, _>(rand::distributions::Uniform::new(0.0, 2.0).unwrap())
+                .take(N_MONTECARLO_SAMPLE);
             iter.filter(|sample| *sample <= y).count()
         })
         .sum::<usize>();
+
     // reduce & print result
-    let res = (count as f64 / (N_STEP * N_SAMPLE) as f64) * 1.0 * 2.0;
+    let res = (count as f64 / (N_SAMPLE * N_MONTECARLO_SAMPLE) as f64) * 1.0 * 2.0;
     println!("integral value of f(x) = 2 * x over [0; 1]: {res}");
 }
