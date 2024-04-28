eb242b7b67898eb92d3436078d8ebe457483725f
imrn99 <github@imrn.fr>
2024-04-28 20:07:30 +0200

    add main methods for the Integraal struct

--- src/compute_unit.rs
+++ src/compute_unit.rs
@@ -3,8 +3,30 @@
 use crate::parameters::ComputeMethod;
 use crate::{DomainDescriptor, FunctionDescriptor};
 
+#[derive(Default)]
 pub struct Integraal {
     domain: Option<DomainDescriptor>,
     function: Option<FunctionDescriptor>,
     method: Option<ComputeMethod>,
+}
+
+impl Integraal {
+    pub fn domain(mut self, domain_descriptor: DomainDescriptor) -> Self {
+        self.domain = Some(domain_descriptor);
+        self
+    }
+
+    pub fn function(mut self, function_descriptor: FunctionDescriptor) -> Self {
+        self.function = Some(function_descriptor);
+        self
+    }
+
+    pub fn method(mut self, compute_method: ComputeMethod) -> Self {
+        self.method = Some(compute_method);
+        self
+    }
+
+    pub fn compute(&self) -> f64 {
+        todo!()
+    }
 }
