fda93a5483b731c7902cd05f37f166eb44088cc9
imrn99 <github@imrn.fr>
2024-10-12 13:47:09 +0200

    add function D tests

--- integraal/src/tests/function_d.rs
+++ integraal/src/tests/function_d.rs
@@ -1,17 +1,65 @@
+// ------ IMPORTS
+
+use super::*;
+
+// ------ TESTS
+
 // integral D (even)
 // y = f(x) = x^2 from -4 to 4
 // expected value = 128/3
 
-mod rectangle_left {}
+mod double {
+    use super::*;
+    const STEP: f64 = 0.001;
+    const RES: f64 = 128.0 / 3.0;
 
-mod rectangle_right {}
+    const RECTANGLE_TOLERANCE: f64 = 1e-5;
+    const TRAPEZOID_TOLERANCE: f64 = 1e-5;
 
-mod trapezoid {}
+    all_tests!(
+        f64,
+        let domain: Vec<f64> = (-4000..4000)
+            .map(|step_id| f64::from(step_id) * STEP)
+            .collect(),
+        FunctionDescriptor::Closure(Box::new(|x: f64| x.powi(2))),
+        FunctionDescriptor::Values(
+            (-4000..4000)
+                .map(|step_id| (f64::from(step_id) * STEP).powi(2))
+                .collect()
+        ),
+        DomainDescriptor::Explicit(&domain),
+        DomainDescriptor::Uniform {
+            start: -4.,
+            step: STEP,
+            n_step: 8000,
+        },
+    );
+}
 
-mod simpson {}
+mod simple {
+    use super::*;
+    const STEP: f32 = 0.001;
+    const RES: f32 = 128.0 / 3.0;
 
-#[cfg(feature = "boole")]
-mod boole {}
+    const RECTANGLE_TOLERANCE: f32 = 1e-5;
+    const TRAPEZOID_TOLERANCE: f32 = 1e-5;
 
-#[cfg(feature = "romberg")]
-mod romberg {}
+    all_tests!(
+        f32,
+        let domain: Vec<f32> = (-4000..4000)
+            .map(|step_id| step_id as f32 * STEP)
+            .collect(),
+        FunctionDescriptor::Closure(Box::new(|x: f32| x.powi(2))),
+        FunctionDescriptor::Values(
+            (-4000..4000)
+                .map(|step_id| (step_id as f32 * STEP).powi(2))
+                .collect()
+        ),
+        DomainDescriptor::Explicit(&domain),
+        DomainDescriptor::Uniform {
+            start: -4.,
+            step: STEP,
+            n_step: 8000,
+        },
+    );
+}
