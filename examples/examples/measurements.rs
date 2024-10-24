d0e0ae63831af5074e31b4d5c659e0a016247154
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-10-24 19:54:04 +0200

    fix parsing

--- examples/examples/measurements.rs
+++ examples/examples/measurements.rs
@@ -4,9 +4,11 @@ use csv::ReaderBuilder;
 use integraal::{ComputeMethod, DomainDescriptor, FunctionDescriptor, Integraal};
 
 fn main() {
-    let (domain, values) = parse_csv("examples/anemometry.csv");
-    println!("{domain:#?}");
-    println!("{values:#?}");
+    let (domain, speed_values) = parse_csv("examples/anemometry.csv");
+
+    // we cannot integrate directly the speed values
+
+    let values = todo!();
 
     let mut integral = Integraal::default()
         .domain(DomainDescriptor::Explicit(&domain))
@@ -36,12 +38,24 @@ fn parse_csv(path: impl AsRef<Path>) -> (Vec<f64>, Vec<f64>) {
         .enumerate()
         .find_map(|(id, s)| if *s == *"U" { Some(id) } else { None })
         .unwrap();
+    println!("r: {radius_col:?}");
+    println!("s: {speed_col:?}");
     reader
         .records()
         .filter_map(|r| {
             match (
-                r.as_ref().unwrap().get(radius_col).unwrap().parse::<f64>(),
-                r.as_ref().unwrap().get(speed_col).unwrap().parse::<f64>(),
+                r.as_ref()
+                    .unwrap()
+                    .get(radius_col)
+                    .unwrap()
+                    .replace(",", ".")
+                    .parse::<f64>(),
+                r.as_ref()
+                    .unwrap()
+                    .get(speed_col)
+                    .unwrap()
+                    .replace(",", ".")
+                    .parse::<f64>(),
             ) {
                 (Ok(v1), Ok(v2)) => Some((v1, v2)),
                 _ => None,
